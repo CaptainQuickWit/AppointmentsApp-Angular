694f79f63d5ad8880d97e999d7f69b0915e4eb96
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-12-14 23:41:43 -0500

    added routes and components on client

---
+++ app/src/app/Appointment.ts
@@ -0,0 +1,7 @@
+export interface Appointment {
+    _id:string;
+    appointmentDate: String;
+    name: string;
+    email: string;
+    
+}
--- app/src/app/appointments.service.ts
+++ app/src/app/appointments.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Appointment } from './Appointment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentsService {
 
-  constructor() { }
+  private BASE_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  getAppointments():Observable<Appointment[]>{
+    return this.http.get<Appointment[]>(`${this.BASE_URL}/appointments`);
+  }
+
+  createAppointment(appointmentDate: string, name: string, email: string): Observable<Appointment> {
+    return this.http.post<Appointment>(`${this.BASE_URL}/appointments`, { appointmentDate, name, email});
+  }
+  //any date returned is fine since we wont do much with it 
+  cancelAppointments(id: string): Observable<any> {
+    return this.http.delete(`${this.BASE_URL}/appointments/${id}`);
+  }
 }
