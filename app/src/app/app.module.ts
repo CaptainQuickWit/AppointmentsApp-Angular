d4b1f5c22dbb7905df249a5014173d3bab8b3b31
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-12-14 23:16:41 -0500

    updated client dependencies version

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,28 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule} from '@angular/forms';
-
-import {
-  MatTableModule,
-  MatProgressSpinnerModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatToolbarModule,
-} from '@angular/material';
-
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AppointmentComponent,
+    AppointmentListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
